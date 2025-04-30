d972c8d61a3d8b714ee4012a3d2c80fe29ffd0f0
Christopher Romero <chris.zeroav@gmail.com>
2025-04-30 10:52:37 -0500

    button for practice added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="min-h-dvh flex items-center justify-center">
+    <div className="min-h-dvh flex flex-col gap-5 items-center justify-center">
       Pagina de Ventifresh F&R en construcción
+      <button className="bg-neutral-800 text-neutral-100 px-6 h-10 rounded">
+        Hazme click
+      </button>
     </div>
   );
 }
